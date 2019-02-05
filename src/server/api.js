48d52b97f7fd65f71b5423377ccb40a1e2d2070a
Max Veytsman <mveytsman@github.com>
2019-02-05 18:45:02 -0500

    Bare bones API

--- src/server/api.js
+++ src/server/api.js
@@ -7,7 +7,39 @@ const router = Router();
 // Example path for `/api/users`
 router.get('/users', catchErrors(usersHandler));
 
+router.get('/messages', catchErrors(messagesHandler));
+
+router.get('/latestMessages', catchErrors(latestMessagesHandler));
+
+router.post('/messages', catchErrors(postMessageHandler));
+
 // Handle calls to non-existent API paths
 router.use('*', handleNotFound);
+
+// TODO: maybe move this to its own file?
+
+let messages = [{message: "hi friends!", username: "Max", timestamp: new Date()}];
+
+function messagesHandler(req, res) {
+   res.json(messages);
+}
+
+function postMessageHandler(req, res) {
+   let username = req.body.username || "anonymous"
+   messages.push({message: req.body.message, username: username, timestamp: new Date()});
+   res.json({success: true})
+}
+
+function latestMessagesHandler(req, res) {
+   let clientMessageCount = req.body.messageCount;
+   let newMessages;
+
+   if (clientMessageCount < messages.length) {
+      newMessages = messages.slice(clientMessageCount)
+   } else {
+      newMessages = [];
+   }
+   res.json(newMessages);
+}
 
 export default router;
--- src/server/controllers/users/index.js
+++ src/server/controllers/users/index.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 async function usersHandler(req, res) {
-  const { data } = await axios.get(
-    'https://jsonplaceholder.typicode.com/users',
-  );
+  // const { data } = await axios.get(
+  //   'https://jsonplaceholder.typicode.com/users',
+  // );
 
-  res.json(data);
+  // res.json(data);
+
+  res.json({"foo": "bar"});
 }
 
 export default usersHandler;
